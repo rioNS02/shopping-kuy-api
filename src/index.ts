f2163f61f32c41dcc3ef4acf4c58af85c23f256f
rioNS02 <rionursamsudin242@gmail.com>
2026-01-25 21:02:00 +0700

    Set up running port

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import { router } from "./routes/usersRouters";
+
+const app = express();
+dotenv.config();
+
+app.use("/", router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Port running in port ${port}`);
+});
