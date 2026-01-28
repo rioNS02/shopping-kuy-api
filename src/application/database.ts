66a4273938e418058b046ed8f2e8b7a78a2605ff
rioNS02 <rionursamsudin242@gmail.com>
2026-01-28 19:33:26 +0700

    Change import location prisma client

--- src/application/database.ts
+++ src/application/database.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "../../src/generated/prisma/client";
 
 const conn = `${process.env.DATABASE_URL}`;
 
