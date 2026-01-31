46007ccc760ebdc4f56a9ead709a4027f37a28c2
rioNS02 <rionursamsudin242@gmail.com>
2026-01-31 13:24:28 +0700

    Change route Auth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { router } from "./routes/usersRouters";
+import { router } from "./routes/authRoute";
 
 const app = express();
 dotenv.config();
