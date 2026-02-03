299ab353e36ecc16491874b6c91b84988a286a27
rioNS02 <rionursamsudin242@gmail.com>
2026-02-03 19:33:44 +0700

    Add helper send email

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,12 +1,31 @@
-import nodemailer from "nodemailer";
+import nodemailer, { type SentMessageInfo } from "nodemailer";
 import "dotenv/config";
 
-export const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+interface Mail {
+  (email: string, subject: string, text: string): Promise<SentMessageInfo>;
+}
+
+export const sendEmail: Mail = async (
+  email: string,
+  subject: string,
+  text: string,
+): Promise<SentMessageInfo> => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: subject,
+    text: text,
+  });
+
+  return mailOptions;
+};
