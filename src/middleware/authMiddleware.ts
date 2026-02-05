3d98ed00e0cae83938dab047c28456aebd28cb1b
rioNS02 <rionursamsudin242@gmail.com>
2026-02-05 20:00:05 +0700

    Add authentication middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,34 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authorization = req.headers.authorization;
+  const token = authorization?.split(" ")[0]!;
+
+  try {
+    if (!authorization)
+      return res.status(401).json({ message: "Unauthorization" });
+
+    const accessToken = process.env.ACCESS_TOKEN;
+
+    if (!accessToken)
+      throw new Error(
+        "FATAL ERROR: JWT SECRET is not defined in env variable.",
+      );
+
+    jwt.verify(token, accessToken, (err, decoded) => {
+      if (err) return res.status(403).json({ message: err });
+
+      req.body.user = decoded;
+
+      next();
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
