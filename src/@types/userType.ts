806df9612b22561d2c8576d311adbecc971143d5
rioNS02 <rionursamsudin242@gmail.com>
2026-02-07 23:15:02 +0700

    Change attribute firstname & lastname zod

--- src/@types/userType.ts
+++ src/@types/userType.ts
@@ -2,8 +2,8 @@ import z from "zod";
 
 export const UserSchema = z.object({
   username: z.string(),
-  first_name: z.string(),
-  last_name: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
   email: z.email(),
   password: z.string().min(8),
   role: z.enum(["ADMIN", "CUSTOMER", "SELLER"]),
