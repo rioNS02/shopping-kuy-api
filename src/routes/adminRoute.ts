816e96967d96ed651fc1bc978d354aee6eee2da8
rioNS02 <rionursamsudin242@gmail.com>
2026-02-09 20:49:48 +0700

    Add middleware route role middleware

--- src/routes/adminRoute.ts
+++ src/routes/adminRoute.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import { getAllUsers, getUsersById } from "../controller/adminController";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { roleMiddleware } from "../middleware/roleMiddleware";
 
 export const router = express.Router();
 
-router.get("/admin/users", getAllUsers);
-router.get("/admin/users/:id", getUsersById);
+router.get(
+  "/admin/users",
+  authMiddleware,
+  roleMiddleware(["ADMIN"]),
+  getAllUsers,
+);
+router.get(
+  "/admin/users/:id",
+  authMiddleware,
+  roleMiddleware(["ADMIN"]),
+  getUsersById,
+);
