b90d019f103e10ba7c81e4ba0d74595cff811eba
rioNS02 <rionursamsudin242@gmail.com>
2026-02-09 16:27:35 +0700

    Feat: Admin controller add get user by Id

--- src/routes/adminRoute.ts
+++ src/routes/adminRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { getAllUsers } from "../controller/adminController";
+import { getAllUsers, getUsersById } from "../controller/adminController";
 
 export const router = express.Router();
 
 router.get("/admin/users", getAllUsers);
+router.get("/admin/users/:id", getUsersById);
