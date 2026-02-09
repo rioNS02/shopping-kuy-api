fbb431035318dbf2867a34b6f682080bd078628d
rioNS02 <rionursamsudin242@gmail.com>
2026-02-09 16:27:22 +0700

    Feat: Admin controller add get user by Id

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -1,15 +1,39 @@
 import type { Request, Response } from "express";
 import { prisma } from "../application/database";
-
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany();
 
-    if (!users) return res.status(404).json({ message: "Users not found" });
+    if (!users)
+      return res
+        .status(404)
+        .json({ success: false, message: "Data user tidak ditemukan." });
 
     return res
       .status(200)
       .json({ success: true, message: "Ambil data user sukses.", data: users });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const getUsersById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const users = await prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!users)
+      return res
+        .status(404)
+        .json({ success: false, message: "Data user tidak ditemukan." });
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Ambil data sukses.", data: users });
   } catch (error) {
     res.status(500).json({ error: error });
   }
