6ede5aacb0f954caadb321494981eec9da5f41a7
rioNS02 <rionursamsudin242@gmail.com>
2026-02-10 19:08:07 +0700

    Refactor: clean up user schema

--- src/@types/userType.ts
+++ src/@types/userType.ts
@@ -1,10 +1,13 @@
 import z from "zod";
 
-export const UserSchema = z.object({
+export const UserBaseSchema = z.object({
   username: z.string(),
   firstName: z.string(),
   lastName: z.string(),
   email: z.email(),
-  password: z.string().min(8),
   role: z.enum(["ADMIN", "CUSTOMER", "SELLER"]),
+});
+
+export const RegisterSchema = UserBaseSchema.extend({
+  password: z.string().min(8),
 });
