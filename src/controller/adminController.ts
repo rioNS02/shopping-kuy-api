36296f6c20a145b144b72fae1b6b60c591630698
rioNS02 <rionursamsudin242@gmail.com>
2026-02-11 20:25:51 +0700

    Feat: add admin controller delete user by username

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -1,19 +1,17 @@
 import type { Request, Response } from "express";
 import { prisma } from "../application/database";
+import { idUserSchema } from "../@types/userType";
+import { Prisma } from "../generated/prisma/client";
+
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany();
-
-    if (!users)
-      return res
-        .status(404)
-        .json({ success: false, message: "Data user tidak ditemukan." });
 
     return res
       .status(200)
       .json({ success: true, message: "Ambil data user sukses.", data: users });
   } catch (error) {
-    res.status(500).json({ error: error });
+    res.status(500).json({ error: "Internal server error." });
   }
 };
 
@@ -35,6 +33,26 @@ export const getUsersById = async (req: Request, res: Response) => {
       .status(200)
       .json({ success: true, message: "Ambil data sukses.", data: users });
   } catch (error) {
-    res.status(500).json({ error: error });
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
+export const deleteUsersByUsername = async (req: Request, res: Response) => {
+  try {
+    const parsed = idUserSchema.parse(req.params);
+    const { username } = parsed;
+    await prisma.user.delete({
+      where: {
+        username,
+      },
+    });
+
+    return res.status(200).json({ message: "OK." });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025")
+        return res.status(404).json({ message: "User not found." });
+    }
+    return res.status(500).json({ message: "Internal server error." });
   }
 };
